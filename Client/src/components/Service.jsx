23bbaf54938931dcd94826640191b1ec07dcbcf2
officialBox <yaseerbostbox@gmail.com>
2024-09-19 14:43:15 +0530

    search bar is added & change something on service section

--- Client/src/components/Service.jsx
+++ Client/src/components/Service.jsx
@@ -13,13 +13,13 @@ const Service = () => {
         <div className="services-list">
           <div className="service-item">
             <FontAwesomeIcon icon={faBook} size="3x" />
-            <h3>Online Book Sales</h3>
-            <p>Browse and purchase books from our extensive online catalog.</p>
+            <h3>Writing Assistance</h3>
+            <p>Guidance on structuring academic papers, writing essays, or preparing research proposals. This may include templates, guides, or tips</p>
           </div>
           <div className="service-item">
             <FontAwesomeIcon icon={faBookReader} size="3x" />
-            <h3>Book Rentals</h3>
-            <p>Rent books for a specified period at affordable rates.</p>
+            <h3>Research Assistance</h3>
+            <p> Students can request help with finding academic sources, databases, or research papers relevant to their study topics. This can include curated resources or links to external academic libraries</p>
           </div>
           <div className="service-item">
             <FontAwesomeIcon icon={faUserCircle} size="3x" />
@@ -28,8 +28,8 @@ const Service = () => {
           </div>
           <div className="service-item">
             <FontAwesomeIcon icon={faUserFriends} size="3x" />
-            <h3>Community Events</h3>
-            <p>Join our community events, including book readings and author signings.</p>
+            <h3>Research Guidance</h3>
+            <p> A step-by-step guide on how to conduct research, with links to databases or reference book downloads.</p>
           </div>
         </div>
         <Testimonials />
--- Client/src/components/TestData.jsx
+++ Client/src/components/TestData.jsx
@@ -1,14 +1,64 @@
 // src/components/TestData.js
-import React from 'react';
-import Data from "../../../Server/data/pdfData.json";
+import React, { useEffect, useState } from 'react';
+import Data from "../../../Server/data/pdfData.json"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import '../css/TestData.css';
 
 const TestData = () => {
+  const [search, setSearch] = useState("");
+  const [book, setBook] = useState(null);
+  
+
+
+  const getData = async () => {
+    const response = Data;
+    console.log(response);
+    let dt = response.slice(0,10)
+    setBook(dt)
+  }
+
+  const handleInput = (e) =>{
+    // console.log(e.target.value);
+    setSearch(e.target.value)
+
+}
+
+  const userInput = (event) =>{
+    setSearch(event.target.value)
+  }
+
+  useEffect(()=>{
+    getData()
+},[])
   return (
     <div className="test-data">
-      {Data.map((post) => (
+      <div className='searchBar'>
+                <input type='text' placeholder='Search News' value={search} onChange={handleInput}/>
+                <button onClick={getData}>Search</button>
+      </div>
+      {
+        Data
+        .filter((val) => {
+          if(search == ""){
+            return val;
+          }else if(val.name.toLowerCase().includes(search.toLowerCase())){
+            return val;
+          }
+        })
+        .map((val) => {
+          return (
+            <div key={val.id} className="test-data-item">
+              <h3>{val.name}</h3>
+              <p>Author: {val.author}</p>
+              <a href={val.pdfURL}  download={val.name} className="download-link">
+                <FontAwesomeIcon icon={faDownload} /> Download
+              </a>
+            </div>
+          );
+        })
+      }
+      {/* {Data.map((post) => (
         <div key={post.id} className="test-data-item">
           <h3>{post.name}</h3>
           <p>Author: {post.author}</p>
@@ -16,7 +66,7 @@ const TestData = () => {
             <FontAwesomeIcon icon={faDownload} /> Download
           </a>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 };
