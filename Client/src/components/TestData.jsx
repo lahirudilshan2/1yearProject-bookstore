64fe8c7788fd55379c7714fa50112b059a675be4
officialBox <yaseerbostbox@gmail.com>
2024-09-20 09:08:51 +0530

    updated or include searchBar

--- Client/src/components/TestData.jsx
+++ Client/src/components/TestData.jsx
@@ -1,72 +1,53 @@
 // src/components/TestData.js
 import React, { useEffect, useState } from 'react';
-import Data from "../../../Server/data/pdfData.json"
+import Data from "../../../Server/data/pdfData.json";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import '../css/TestData.css';
 
 const TestData = () => {
   const [search, setSearch] = useState("");
-  const [book, setBook] = useState(null);
-  
 
-
-  const getData = async () => {
-    const response = Data;
-    console.log(response);
-    let dt = response.slice(0,10)
-    setBook(dt)
+  const handleInput = (e) => {
+    setSearch(e.target.value);
   }
 
-  const handleInput = (e) =>{
-    // console.log(e.target.value);
-    setSearch(e.target.value)
+  const handleSearchClick = () => {
+    // You can add any specific search click logic here
+    // Or reset the search input after clicking the button if needed
+    // setSearch("");
+  };
 
-}
-
-  const userInput = (event) =>{
-    setSearch(event.target.value)
-  }
-
-  useEffect(()=>{
-    getData()
-},[])
   return (
     <div className="test-data">
       <div className='searchBar'>
-                <input type='text' placeholder='Search News' value={search} onChange={handleInput}/>
-                <button onClick={getData}>Search</button>
+        <input
+          type='text'
+          placeholder='Enter Title'
+          value={search}
+          onChange={handleInput}
+        />
+        <button onClick={handleSearchClick}>Search</button>
       </div>
       {
-        Data
-        .filter((val) => {
-          if(search == ""){
+        Data.filter((val) => {
+          if (search === "") {
             return val;
-          }else if(val.name.toLowerCase().includes(search.toLowerCase())){
+          } else if (val.name.toLowerCase().includes(search.toLowerCase())) {
             return val;
           }
-        })
-        .map((val) => {
+        }).map((val) => {
           return (
             <div key={val.id} className="test-data-item">
               <h3>{val.name}</h3>
               <p>Author: {val.author}</p>
-              <a href={val.pdfURL}  download={val.name} className="download-link">
+              <a href={val.pdfURL} download={val.name} className="download-link">
                 <FontAwesomeIcon icon={faDownload} /> Download
               </a>
             </div>
           );
         })
       }
-      {/* {Data.map((post) => (
-        <div key={post.id} className="test-data-item">
-          <h3>{post.name}</h3>
-          <p>Author: {post.author}</p>
-          <a href={post.pdfURL} download={post.name} className="download-link">
-            <FontAwesomeIcon icon={faDownload} /> Download
-          </a>
-        </div>
-      ))} */}
     </div>
   );
 };
